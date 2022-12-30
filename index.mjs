a962269b7c85b297c4abba1aca45fa637db09f2f
Rhyker <a.rhykerw@gmail.com>
2022-12-30 23:04:54 +0000

    fix(Comments): Consistent section naming

--- index.mjs
+++ index.mjs
@@ -13,20 +13,23 @@ app.get('/welcome', async (req, res) => {
     const background = await loadImage('https://media.discordapp.net/attachments/913966196758564874/1058121547224731769/image.png');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
+    // Avatar
     const avatar = await loadImage('https://cdn.discordapp.com/avatars/765316548516380732/1ee9def1c460ef2e28f7572e4491dab4.png?size=256');
 
+    // Border
     ctx.scale(2, 2)
     ctx.beginPath();
     ctx.arc(canvas.width / 4, canvas.height / 6.5, 60, 0, 2 * Math.PI);
     ctx.lineWidth = 7;
     ctx.stroke();
 
+    // PFP Display
     ctx.save();
     ctx.beginPath();
     ctx.arc(canvas.width / 4, canvas.height / 6.5, 60, 0, 2 * Math.PI);
     ctx.clip();
     ctx.drawImage(avatar, canvas.width / 4 - 60, canvas.height / 6.5 - 60, 128, 128);
-    
+
     // Output
     const buffer = canvas.toBuffer('image/png')
     writeFileSync('./output/output.png', buffer)
